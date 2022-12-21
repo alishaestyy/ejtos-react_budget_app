bb1684e211d13bfe368499ce5df8db4678920a8c
alishaestyy <alishaesteves27@gmail.com>
2022-12-21 03:56:09 -0500

    functionality added

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,19 @@
+/*In Budget.js you will be adding text and value for your budget. 
+You will be importing app context and the useContext hook, and pass 
+your AppContext to it - this is how a component connects to the context 
+in order to get values from global state */
 
+import React, {useContext} from 'react';
+import {AppContext} from '../context/AppContext';
+
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: ${budget}</span>
+        </div>
+    );
+};
+
+export default Budget;
